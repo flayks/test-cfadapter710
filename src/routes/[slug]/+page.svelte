3b0cfbbe8eae0b4a8d15e3d0eda9b07bdf930fd0
Félix Péault <hello@flayks.com>
2025-07-21 14:30:43 -0700

    test dynamic routes + svelte preprocess + scss

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,8 +1,12 @@
+<script lang="ts">
+    import { page } from '$app/state'
+</script>
+
 <style lang="scss">
     h1 {
         color: purple;
     }
 </style>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to /{page.params.slug}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
